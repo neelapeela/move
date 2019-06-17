1b5a3a3e02ac569a2bc753173c1d0a23e7cd778e
Neel Madhav <51863122+neelapeela@users.noreply.github.com>
2019-06-17 10:07:15 +0530

    Add files via upload
    well functioning movement constraining procedural path

---
+++ restrictionInst.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restrictionInst : MonoBehaviour
+{
+    public GameObject rest;
+    Vector3 left;
+    Vector3 right;
+    Vector3 up;
+    Vector3 down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        left = new Vector3(transform.position.x -1, transform.position.y + 1, transform.position.z );
+        right = new Vector3(transform.position.x +1, transform.position.y + 1, transform.position.z);
+        up = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z -1);
+        down = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 1);
+        Instantiate(rest, left, transform.rotation);
+        Instantiate(rest, right, transform.rotation);
+        Instantiate(rest, up, transform.rotation);
+        Instantiate(rest, down, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
